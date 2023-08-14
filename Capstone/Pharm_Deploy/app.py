fe2938cf26495a349b44d95ba3038279aaa8a437
Kevin Jin <kevinjin21@gmail.com>
2023-08-13 21:56:06 -0500

    HTML and CSS update for web application

--- Capstone/Pharm_Deploy/app.py
+++ Capstone/Pharm_Deploy/app.py
@@ -2,7 +2,7 @@
 
 import pickle
 import numpy as np
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 model = None
 app = Flask(__name__)
@@ -14,19 +14,29 @@ def load_model():
         model = pickle.load(f)
 
 
+def inverse_norm(result):
+    with open('norm_weights.pkl', 'rb') as f:
+        data = pickle.load(f)
+        ceil = data[0]
+        floor = data[1]
+        return result*(ceil-floor) + floor
+
+
 @app.route('/')
 def home_endpoint():
-    return 'Welcome to Pharmaceutical Prediction App'
+    return render_template('home.html')
 
 
 @app.route('/predict', methods=['POST'])
 def get_prediction():
     if request.method == 'POST':
-        data = request.get_json()
-        data = np.array(data['data'])[np.newaxis, :]
-        #test_array = np.array(data['data'])
-        prediction = model.predict(data)
-    return str(prediction[0])
+        features = []
+        for i in range(10):
+            features.append(float(request.form[f"feat_{i+1}"]))
+        data = np.array(features)
+        prediction = model.predict(data.reshape(1, -1))
+        prediction = inverse_norm(prediction)
+        return render_template('predict.html', pred=round(prediction[0], 3))
 
 
 if __name__ == '__main__':
